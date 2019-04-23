c1dad6bd7468a96acaa291db33503916391e4df9
samtoxie <samtoxie@live.com>
2019-04-23 19:16:55 +0200

    Initial commit

---
+++ Callouts/FakeCall.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using Rage;
+using Rage.Native;
+using System.Diagnostics;
+using System.IO;
+using Basic_Callouts.Callouts;
+
+namespace Basic_Callouts.Callouts
+{
+    [CalloutInfo("Suspicious Activity", CalloutProbability.Medium)]
+    public class FakeCall : Callout
+    {
+        //ASSETS
+
+        private Ped Suspect;
+        private Vehicle SuspectVehicle;
+        private Vector3 SpawnPoint;
+        private Blip SuspectBlip;
+
+        //CALLOUT MESSAGE
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            SpawnPoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(900f)); ShowCalloutAreaBlipBeforeAccepting(SpawnPoint, 30f); AddMinimumDistanceCheck(20f, SpawnPoint);
+            CalloutMessage = "Suspicious Activity"; CalloutPosition = SpawnPoint;
+            Functions.PlayScannerAudio("CITIZENS_REPORT CRIME_SUSPICIOUS_ACTIVITY IN_OR_ON_POSITION");
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+
+        public override bool OnCalloutAccepted()
+        {
+            Functions.PlayScannerAudio("UNIT_TAKING_CALL_01 REPORT_RESPONSE_COPY_03");
+            Functions.PlayScannerAudio("BEAT_03 XRAY DIV_08 UNITS_RESPOND_CODE_03_02");
+            SuspectVehicle = new Vehicle("POLICE", SpawnPoint);
+            SuspectVehicle.IsPersistent = true;
+            Suspect = SuspectVehicle.CreateRandomDriver();
+            Suspect.IsPersistent = true;
+            Suspect.BlockPermanentEvents = true;
+            SuspectBlip = Suspect.AttachBlip();
+            SuspectBlip.IsFriendly = true;
+            SuspectBlip.Color = System.Drawing.Color.Red;
+            SuspectBlip.EnableRoute(System.Drawing.Color.Red);
+            return base.OnCalloutAccepted();
+        }
+
+        public override void Process()
+        {
+            base.Process();
+            if (Game.LocalPlayer.Character.DistanceTo(Suspect.Position) < 60f)
+            { Game.DisplaySubtitle("~b~Dispatch~w~: Disregard, callout is a prankcall.", 4000); GameFiber.Wait(1000); End(); }
+        }
+
+        public override void End()
+        {
+            base.End();
+            Functions.PlayScannerAudio("WE_ARE_CODE_4");
+            if (Suspect.Exists()) { Suspect.Dismiss(); }
+            if (SuspectVehicle.Exists()) { SuspectVehicle.Dismiss(); }
+            if (SuspectBlip.Exists()) { SuspectBlip.Delete(); }
+
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,56 @@
+﻿using LSPD_First_Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using Basic_Callouts;
+
+namespace Basic_Callouts
+{
+    public class Main : Plugin
+    {
+        //Initialization of the plugin.
+        public override void Initialize()
+        {
+            //This is saying that when our OnDuty status is changed, it calls for the code to call private static void OnOnDutyStateChangedHandler near the bottom of this page.
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+
+            //Game.LogTrivial's are used to help you identify problems when debugging a crash with your plugin, so you know exactly what's going on when.
+
+            //This will show in the RagePluginHook.log as "Example Plugin 1.0.0.0 has been initialised." 
+            Game.LogTrivial("Basic Callouts " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " has been initialised.");
+
+            //This one will show in RagePluginHook.log as "Go on duty to fully load Example Plugin."
+            Game.LogTrivial("Go on duty to fully load Basic Callouts.");
+        }
+        //This is a simple message saying that Example Plugin has been cleanup.
+        public override void Finally()
+        {
+            Game.LogTrivial("Basic Callouts has been cleaned up.");
+        }
+        //This is called by Functions.OnOnDutyStateChanged as stated above, but only when bool OnDuty is true.
+        private static void OnOnDutyStateChangedHandler(bool OnDuty)
+        {
+            //We have to make sure they are actually on duty so the code can do its work, so we use an "if" statement.
+            if (OnDuty)
+            {
+                //This calls for the method private static void RegisterCallouts() down below.
+                RegisterCallouts();
+
+                //This shows a notification at the bottom left, above the minimap, of your screen when you go on duty, stating exactly what's in the quotation marks.
+                Game.DisplayNotification("Basic Callouts has loaded successfully, thank you for downloading!");
+            }
+        }
+        //This is the method that we called earlier in private static void OnOnDutyStateChangedHandler. This registers the callouts we have it setup to register, we'll come back to this after we make our callout.
+        private static void RegisterCallouts()
+        {
+            Functions.RegisterCallout(typeof(Callouts.FakeCall));
+        }
+    }
+}
